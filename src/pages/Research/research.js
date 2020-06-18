6a5c5a3ae2ca390f41a4f60e93e4d96847dbd006
Mikihisa Yuasa <mikihisa.yuasa@wisc.edu>
2020-06-18 18:03:34 -0500

    edits

--- src/pages/Research/research.js
+++ src/pages/Research/research.js
@@ -15,8 +15,8 @@ export default function Research() {
                 <p>I was widely interested in aerodynamics and collaborative unmanned vehicles in the context of integrated intelligent transportation where both aerial, ground, and marine automated vehicles require optimizations of the design in terms of fluid dynamics and controls on the interactions of vehicles. This interest led me to work in two laboratories: one is computational fluid dynamics lab, and the other is connected automated vehicles and highway lab.</p>
                 <p>Some topics on which I have worked with Professor Jennifer Franck in the computational fluid dynamics lab are:</p>
                 <ol>
-                    <li>Development and implementation of a CFD software solver library (OpenFOAM) for dynamic mesh generation for seal whiskers.</li>
-                    <li>Development of a MATLAB<sup>®</sup> library of Schwartz-Christoffel conformal mesh-generation method for 2D geometry.</li>
+                    <li>Development and implementation of dynamic mesh generation library for seal whiskers on a CFD solver (OpenFOAM).</li>
+                    <li>Development of a mesh-generation library using Schwartz-Christoffel conformal mapping for 2D geometries on MATLAB<sup>®</sup> .</li>
                 </ol>
                 <p>Another set of topics I have worked with Professor Bin Ran in the connected automated vehicles and highway lab are:</p>
             </div>
