17aaab56b4940d79db4d46d30953bcc24cd9cf7c
Mikihisa Yuasa <mikihisa.yuasa@wisc.edu>
2020-06-20 22:56:01 -0500

    edits

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -26,9 +26,9 @@ export default function About() {
                 <table >
                     <tbody valign='top'>
                         <tr ><td>Name:</td><td>Mikihisa Yuasa</td></tr>
-                        <tr ><td>School:</td><td>Junior undergraduate at the Department of Engineering-Mechanics, College of Engineering, University of Wisconsin-Madison </td></tr>
-                        <tr ><td>Labs:</td><td>J. Franck Computational Fluid Dynamics Lab<br /> B. Ran Connected Automated Vehicles and Highway Lab</td></tr>
-                        <tr><td>Keywords:</td><td>Computational fluid dynamics, Mesh generation, Flow control, Connected automated vehicles and highway, Aerial/ground unmanned vehicles, Intelligent transportation, Computing, Harbor seal whisker, Conformal mapping</td></tr>
+                        <tr ><td>School:</td><td>Senior undergraduate at the Department of Engineering-Mechanics, College of Engineering, University of Wisconsin-Madison </td></tr>
+                        <tr ><td>Labs:</td><td>J. Franck Computational Fluid Dynamics (CFD) Lab<br /> B. Ran Connected Automated Vehicles and Highway (CAVH) Lab</td></tr>
+                        <tr><td>Keywords:</td><td>Dynamic Control, Computational Fluid Dynamics, Mesh Generation, Flow Control, Connected Automated Vehicles and Highway, Aerial/Ground Unmanned Vehicles, Intelligent Transportation, Computing, Biomimicry, Harbor Seal Whisker, Conformal Mapping</td></tr>
                         <tr><td>Email:</td><td>mikihisa(dot)yuasa(at)wisc(dot)edu</td></tr>
                         <tr><td>CV:</td><td><Link to="../page-2" style={{ color: theme.palette.neutralLight }}><strong>Download from Here!</strong></Link></td></tr>
                     </tbody>
--- src/pages/Research/research.js
+++ src/pages/Research/research.js
@@ -9,12 +9,18 @@ export default function Research() {
             </div>
             <div class='sectionBody'>
                 <p>I was widely interested in aerodynamics and collaborative unmanned vehicles in the context of integrated intelligent transportation where both aerial, ground, and marine automated vehicles require optimizations of the design in terms of fluid dynamics and controls on the interactions of vehicles. This interest led me to work in two laboratories: one is computational fluid dynamics lab, and the other is connected automated vehicles and highway lab.</p>
-                <p>Some topics on which I have worked with Professor Jennifer Franck in the computational fluid dynamics lab are:</p>
+                <p>Some topics on which I have worked with Professor Jennifer Franck in the CFD lab are:</p>
                 <ol>
-                    <li>Development and implementation of dynamic mesh generation library for seal whiskers on a CFD solver (OpenFOAM).</li>
-                    <li>Development of a mesh-generation library using Schwartz-Christoffel conformal mapping for 2D geometries on MATLAB<sup>®</sup> .</li>
+                    <li>Dynamic Mesh Generation: developed and implemented a dynamic mesh generation library for harbor seal whiskers for a CFD solver (OpenFOAM).</li>
+                    <li>Flow Control: evaluated the influece of geometry transformation of harbor seal whiskers on hydrodynamic reactions.</li>
+                    <li>Comformal Mapping: developed a mesh-generation library using Schwartz-Christoffel conformal mapping for 2D geometries on MATLAB<sup>®</sup> .</li>
                 </ol>
-                <p>Another set of topics I have worked with Professor Bin Ran in the connected automated vehicles and highway lab are:</p>
+                <p>Another set of topics I have worked with Professor Bin Ran in the CAVH lab are:</p>
+                <ol>
+                    <li>Mixed Traffic: built simulation models to evaluate how platooned automated vehicles can improve the network capacity</li>
+                    <li>Car-following Model: investigated the way to dynamically assign costs for indivicual vehicles to join and leave platoons.</li>
+                    <li>Weather Condition: simulated how the network capacity of highway can change under the snow.</li>
+                </ol>
             </div>
         </div>
     )
