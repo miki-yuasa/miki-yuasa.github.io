768be4e8231c7beb2d849b97c295e7024d3584cc
Mikihisa Yuasa (WSL2) <miki@DESKTOP-VMJ21UJ.localdomain>
2020-07-17 16:33:56 -0500

    added intersection observer

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,38 +20,86 @@ const pivotStyles: IPivotStyles = {
   icon: {}
 };
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      boxShadow: Depths.depth16,
-    }}
-  >
-    <div className='headerPivot'>
-      <Pivot
-        aria-label="Basic Pivot Example"
-        styles={pivotStyles}
-        linkSize={PivotLinkSize.large}
-        headersOnly>
-        <PivotItem
-          headerText="Home"
-          headerButtonProps={{ href: "/#home" }} />
-        <PivotItem
-          headerText="About"
-          headerButtonProps={{ href: "/#about" }} />
-        <PivotItem
-          headerText="Research"
-          headerButtonProps={{ href: "/#research" }} />
-        <PivotItem
-          headerText="Software"
-          headerButtonProps={{ href: "/#software" }} />
-        {/*
+function Header() {
+  const [selectedKey, setSelectedKey] = React.useState({});
+
+  const options = {
+    root: null, // 今回はビューポートをルート要素とする
+    rootMargin: "-50% 0px", // ビューポートの中心を判定基準にする
+    threshold: 0 // 閾値は0
+  };
+
+  if (typeof document !== 'undefined') {
+    const boxes = document.querySelectorAll(".idBox");
+    const observer = new IntersectionObserver(doWhenIntersect, options);
+    boxes.forEach(box => {
+      observer.observe(box);
+    });
+    // それぞれのboxを監視する
+
+    function doWhenIntersect(entries) {
+      // 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          activateIndex(entry.target);
+        }
+      });
+    }
+
+    function activateIndex(element) {
+      if (element.id == 'home') {
+        setSelectedKey(0);
+      }
+      else if (element.id == 'about') {
+        setSelectedKey(1);
+      }
+      else if (element.id == 'research') {
+        setSelectedKey(2);
+      }
+      else if (element.id == 'software') {
+        setSelectedKey(3);
+      }
+    }
+  }
+
+  return (
+    <header
+      style={{
+        boxShadow: Depths.depth16,
+      }}
+    >
+      <div className='headerPivot'>
+        <Pivot
+          aria-label="Basic Pivot Example"
+          styles={pivotStyles}
+          linkSize={PivotLinkSize.large}
+          headersOnly
+          selectedKey={String(selectedKey)}>
+          <PivotItem
+            headerText="Home"
+            headerButtonProps={{ href: "/#home" }}
+            itemKey="0" />
+          <PivotItem
+            headerText="About"
+            headerButtonProps={{ href: "/#about" }}
+            itemKey="1" />
+          <PivotItem
+            headerText="Research"
+            headerButtonProps={{ href: "/#research" }}
+            itemKey="2" />
+          <PivotItem
+            headerText="Software"
+            headerButtonProps={{ href: "/#software" }}
+            itemKey="3" />
+          {/*
         <PivotItem
           headerText="Contact"
           headerButtonProps={{ href: "/#contact" }} />
            */}
-      </Pivot>
-    </div>
-  </header >
-)
+        </Pivot>
+      </div>
+    </header >
+  )
+}
 
 export default Header
