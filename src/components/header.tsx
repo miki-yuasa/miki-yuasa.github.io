5730f4c1108b8820dad81fbf5cd5a144ba3394e5
Mikihisa Yuasa (WSL2) <miki@DESKTOP-VMJ21UJ.localdomain>
2020-07-17 23:00:35 -0500

    added page data to the header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
 import React from "react"
-import { Pivot, PivotItem, PivotLinkSize, IPivotStyles } from 'office-ui-fabric-react/lib/Pivot';
+import { getTheme } from '@fluentui/react';
+import { mergeStyles } from 'office-ui-fabric-react/lib/Styling';
 import { Depths } from '@uifabric/fluent-theme/lib/fluent/FluentDepths';
+import { Pivot, PivotItem, PivotLinkSize, IPivotStyles } from 'office-ui-fabric-react/lib/Pivot';
+import { ActionButton, IContextualMenuProps, IButtonStyles } from 'office-ui-fabric-react';
+import { registerIcons } from 'office-ui-fabric-react/lib/Styling';
+import { FontIcon } from 'office-ui-fabric-react/lib/Icon';
 
+import { getPageData } from '../data/pageData'
 import "./layout.css"
 
 const pivotStyles: IPivotStyles = {
@@ -19,6 +25,68 @@ const pivotStyles: IPivotStyles = {
   text: {},
   icon: {}
 };
+
+const theme = getTheme();
+
+const menuIconClass = mergeStyles({
+  height: 35,
+  width: 35,
+  padding: '7px 7px 0 -5px',
+});
+
+const menuProps: IContextualMenuProps = {
+  items: [],
+  styles: {
+    root: {
+      marginRight: '-90px',
+      paddingRight: '0px'
+    }
+  }
+};
+
+const menuButtonStyles: IButtonStyles = {
+  root: {
+    boxShadow: Depths.depth64,
+    padding: '0 auto',
+    width: '45px',
+    background: theme.palette.neutralLighterAlt,
+    stroke: theme.palette.neutralPrimary,
+  },
+  rootHovered: {
+    stroke: theme.palette.themePrimary,
+  },
+  rootExpanded: {
+    stroke: theme.palette.themePrimary,
+  },
+  rootPressed: {
+    stroke: theme.palette.neutralPrimary,
+  }
+}
+
+registerIcons({
+  icons: {
+    'hamburger': (
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0,0,512,512'>
+        <g fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="32px" strokeMiterlimit='10'>
+          <line x1='80' y1='160' x2='432' y2='160' />
+          <line x1='80' y1='256' x2='432' y2='256' />
+          <line x1='80' y1='352' x2='432' y2='352' />
+        </g>
+      </svg>
+    ),
+  },
+});
+
+const pageData = getPageData();
+
+for (let i in pageData) {
+  menuProps.items.push(
+    {
+      key: pageData[i].name,
+      text: pageData[i].name,
+      href: "/#" + pageData[i].name.toLowerCase()
+    })
+}
 
 function Header() {
   const [selectedKey, setSelectedKey] = React.useState({});
