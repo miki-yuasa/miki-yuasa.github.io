d5794b65fc505a6c20016febc23cd7493b26b7f2
Mikihisa Yuasa (WSL2) <mikihisa.yuasa@wisc.edu>
2020-08-21 18:19:18 -0500

    type set

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import { Depths } from '@uifabric/fluent-theme/lib/fluent/FluentDepths';
+import React from "react";
+import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
 
-import NavWide from '../components/navs/navWide'
-import NavCompact from '../components/navs/navCompact'
-import { getPageData } from '../data/pageData'
-import "./layout.css"
+import NavWide from "../components/navs/navWide";
+import NavCompact from "../components/navs/navCompact";
+import { getPageData } from "../data/pageData";
+import "./layout.css";
 
-function Header() {
+const Header = () => {
   const [selectedKey, setSelectedKey] = React.useState({});
 
   const options = {
     root: null, // 今回はビューポートをルート要素とする
     rootMargin: "-50% 0px", // ビューポートの中心を判定基準にする
-    threshold: 0 // 閾値は0
+    threshold: 0, // 閾値は0
   };
 
   const pageData = getPageData();
@@ -21,39 +21,39 @@ function Header() {
     pageIndexNames[i] = pageData[i].name.toLowerCase();
   }
 
-  if (typeof document !== 'undefined') {
+  if (typeof document !== "undefined") {
     const boxes = document.querySelectorAll(".idBox");
     const observer = new IntersectionObserver(doWhenIntersect, options);
-    boxes.forEach(box => {
+    boxes.forEach((box) => {
       observer.observe(box);
     });
     // それぞれのboxを監視する
+  }
 
-    function doWhenIntersect(entries) {
-      // 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          activateIndex(entry.target);
-        }
-      });
-    }
+  function doWhenIntersect(entries: IntersectionObserverEntry[]) {
+    // 交差検知をしたもののなかで、isIntersectingがtrueのDOMを色を変える関数に渡す
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        activateIndex(entry.target);
+      }
+    });
+  }
 
-    function activateIndex(element) {
-      const pageKey: number = pageIndexNames.indexOf(element.id);
-      setSelectedKey(pageKey)
-    }
+  function activateIndex(element: Element) {
+    const pageKey: number = pageIndexNames.indexOf(element.id);
+    setSelectedKey(pageKey);
   }
 
   return (
     <>
-      <header className='compact'>
+      <header className="compact">
         <NavCompact />
-      </header >
-      <header className='wide' style={{ boxShadow: Depths.depth64 }}>
+      </header>
+      <header className="wide" style={{ boxShadow: Depths.depth64 }}>
         <NavWide selectedKey={String(selectedKey)} />
-      </header >
+      </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
