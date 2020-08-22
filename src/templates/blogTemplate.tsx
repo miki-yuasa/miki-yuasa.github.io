2ba8fd0d4a9ca1e6a3ac403b6e51694f53fb1540
Mikihisa Yuasa (WSL2) <mikihisa.yuasa@wisc.edu>
2020-08-22 11:55:58 -0500

    type edit

--- src/templates/blogTemplate.tsx
+++ src/templates/blogTemplate.tsx
@@ -2,29 +2,26 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import { BlogHeader } from "../components/blog/blogHeader";
-import {
-  QueryMarkdownRemarkArgs,
-  Query,
-  QueryAllMarkdownRemarkArgs,
-} from "../../@types/graphql-types";
+import { BlogArticleBySlugQuery } from "../../@types/graphql-types";
 
 const blogArticleTemplate = ({
   data,
   pageContext,
 }: {
-  data: any;
+  data: BlogArticleBySlugQuery;
   pageContext: any;
 }) => {
   const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+  const frontmatter = markdownRemark?.frontmatter;
+  const html = markdownRemark?.html;
   return (
     <div className="blog-post-container">
       <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+        <h1>{frontmatter?.title}</h1>
+        <h2>{frontmatter?.date}</h2>
         <div
           className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
+          dangerouslySetInnerHTML={{ __html: html! }}
         />
       </div>
     </div>
