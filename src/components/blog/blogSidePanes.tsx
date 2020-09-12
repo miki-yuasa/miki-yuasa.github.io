7c374f8787e057c759441221845f6cf0b8d24404
Mikihisa Yuasa (WSL2) <mikihisa.yuasa@wisc.edu>
2020-09-11 22:51:46 -0500

    added archive pane

--- src/components/blog/blogSidePanes.tsx
+++ src/components/blog/blogSidePanes.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import { BlogSideTagList } from "./blogSideTagList";
+import { BlogSideArchiveList } from "./blogSideArchiveList";
 
 export const BlogSidePanes = () => {
   return (
     <>
       <div className="articleSidePane">
         <BlogSideTagList />
+      </div>
+      <div className="articleSidePane">
+        <BlogSideArchiveList />
       </div>
     </>
   );
