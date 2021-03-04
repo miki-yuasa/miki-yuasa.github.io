15214024f0c243e65d166bec74f41e435b290e3b
Mikihisa Yuasa <mikihisa.yuasa@wisc.edu>
2021-03-04 11:12:36 -0600

    package deletion

--- @types/graphql-types.d.ts
+++ @types/graphql-types.d.ts
@@ -3190,14 +3190,6 @@ type BlogTagListQueryVariables = Exact<{ [key: string]: never; }>;
 
 type BlogTagListQuery = { readonly allMdx: { readonly group: ReadonlyArray<Pick<MdxGroupConnection, 'fieldValue' | 'totalCount'>> } };
 
-type ImageQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type ImageQuery = { readonly images: { readonly edges: ReadonlyArray<{ readonly node: (
-        Pick<File, 'relativePath' | 'name'>
-        & { readonly childImageSharp: Maybe<{ readonly fluid: Maybe<GatsbyImageSharpFluidFragment> }> }
-      ) }> } };
-
 type BlogCardListQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -3206,10 +3198,23 @@ type BlogCardListQuery = { readonly allMdx: { readonly edges: ReadonlyArray<{ re
           & { readonly image: Maybe<{ readonly childImageSharp: Maybe<{ readonly fluid: Maybe<GatsbyImageSharpFluid_withWebp_tracedSVGFragment> }> }> }
         )> } }> } };
 
+type ImageQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type ImageQuery = { readonly images: { readonly edges: ReadonlyArray<{ readonly node: (
+        Pick<File, 'relativePath' | 'name'>
+        & { readonly childImageSharp: Maybe<{ readonly fluid: Maybe<GatsbyImageSharpFluidFragment> }> }
+      ) }> } };
+
 type Unnamed_1_QueryVariables = Exact<{ [key: string]: never; }>;
 
 
 type Unnamed_1_Query = { readonly desktop: Maybe<{ readonly childImageSharp: Maybe<{ readonly fluid: Maybe<GatsbyImageSharpFluid_withWebpFragment> }> }> };
+
+type Unnamed_2_QueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type Unnamed_2_Query = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'description' | 'author' | 'url'>> }> };
 
 type infoAndSiteTitleQueryQueryVariables = Exact<{ [key: string]: never; }>;
 
@@ -3218,11 +3223,6 @@ type infoAndSiteTitleQueryQuery = { readonly site: Maybe<(
     Pick<Site, 'buildTime'>
     & { readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title'>> }
   )> };
-
-type Unnamed_2_QueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type Unnamed_2_Query = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'description' | 'author' | 'url'>> }> };
 
 type GatsbyImageSharpFixedFragment = Pick<ImageSharpFixed, 'base64' | 'width' | 'height' | 'src' | 'srcSet'>;
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -66,24 +66,6 @@ module.exports = {
             },
           },
           {
-            resolve: `gatsby-remark-image-attributes`,
-            options: {
-
-              // ?Boolean=true
-              //   If true (the default), all CSS
-              //   property names will be recognized
-              //   as styleAttribute.
-              styleAttributes: true,
-
-              // ?Boolean=false
-              //   If true, all attributes that
-              //   aren't styleAttributes, will be
-              //   added as data-* attributes to the
-              //   image.
-              dataAttributes: false
-            }
-          },
-          {
             resolve: `gatsby-remark-classes`,
             options: {
               classMap: {
