e91da39ab2486d00d39204bad44827328707e29d
miki-yuasa <miki-yuasa@users.noreply.github.com>
2021-03-16 02:17:50 +0000

    deploy: 618ccbc343c12038998abc6cbe99ec8db334fa6b

---
+++ component---src-pages-404-js-0f1d6595e96d2d62974d.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmikihisa_yuasa_portfolio=self.webpackChunkmikihisa_yuasa_portfolio||[]).push([[883],{159616:function(e,t,a){"use strict";a.r(t);var n=a(667294),u=a(522512),l=a(143240);t.default=function(){return n.createElement(u.Z,null,n.createElement(l.Z,{title:"404: Not found"}),n.createElement("h1",null,"NOT FOUND"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0f1d6595e96d2d62974d.js.map
