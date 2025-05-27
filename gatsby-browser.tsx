8ca54ac82025c6698bfe61a204a7afe608149830
Mikihisa Yuasa <mik0915yua@outlook.com>
2025-05-27 13:22:49 -0500

    feat: add darkmode

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme } from '@mui/material/styles';
+import { createTheme, extendTheme } from '@mui/material/styles';
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 
-const theme = createTheme({
+const theme = extendTheme({
     breakpoints: {
         values: {
             xs: 0,
@@ -15,31 +16,52 @@ const theme = createTheme({
     },
     // You can add palette, typography, etc. here as needed
     // Color palette example
-    palette: {
-        mode: 'light', // or 'dark'
-        primary: {
-            main: '#0095d9', // Ao blue
-            light: "'#0094c8'"
+    colorSchemes: {
+        light: {
+            palette: { // mode: 'dark',// or 'dark'
+                primary: {
+                    main: '#007bbb', // Tsuyukusa blue
+                },
+                secondary: {
+                    main: '#a22041', // Shinku red
+                },
+                warning: {
+                    main: '#f8b500', // Yamabuki yellow
+                },
+                info: {
+                    main: '#59b9c6', // Shimbashi blue
+                },
+                success: {
+                    main: '#028760', // Tokiwa midori green
+                },
+            }
         },
-        secondary: {
-            main: '#a22041', // Shinku red
-        },
-        warning: {
-            main: '#f8b500', // Yamabuki yellow
-        },
-        info: {
-            main: '#59b9c6', // Shimbashi blue
-        },
-        success: {
-            main: '#028760', // Tokiwa midori green
-        },
-    },
+        dark: {
+            palette: { // mode: 'dark',// or 'dark'
+                primary: {
+                    main: '#a0d8ef ', // Tsuyukusa blue
+                },
+                secondary: {
+                    main: '#a22041', // Shinku red
+                },
+                warning: {
+                    main: '#f8b500', // Yamabuki yellow
+                },
+                info: {
+                    main: '#59b9c6', // Shimbashi blue
+                },
+                success: {
+                    main: '#028760', // Tokiwa midori green
+                },
+            }
+        }
+    }
 });
 
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme} defaultMode="system">
         <CssBaseline />
         {element}
-    </ThemeProvider>
+    </CssVarsProvider>
 );
 
---
+++ gatsby-ssr.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { getInitColorSchemeScript } from '@mui/material/styles';
+
+export const onRenderBody = ({ setHtmlAttributes, setPreBodyComponents, setBodyAttributes }) => {
+    setPreBodyComponents([
+        <React.Fragment key="mui-init-color-scheme-script">
+            {getInitColorSchemeScript({ defaultMode: "system" })}
+        </React.Fragment>,
+    ]);
+    // setHtmlAttributes({ lang: "ja" })
+}
